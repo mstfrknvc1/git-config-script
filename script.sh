a8874b13c2b54dbbe78b3cb64384e58a89cbcb90
mstfrknvc1 <mstfrknvc@outlook.com>
2024-10-07 18:25:59 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,8 +2,8 @@ sudo apt install git g++ make #git g++ ve make gereçleri yüklenir.
 sudo snap install code --classic #vscode yüklenir
 #ad soyad ve e posta bilgileri alınır ve confige girilir 
 echo -n "Adınız: "
-read -t 4 user
+read -t 60 user
 echo -n "E-postanız: "
-read -t 4 eposta
+read -t 60 eposta
 git config --global user.name "$user"
 git config --global user.email "$eposta"
